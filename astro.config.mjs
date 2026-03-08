1675175b3a4510cc947959d3401bc7f2c8c843f6
John Schmidt <mail@johnschmidt.de>
2026-03-08 21:14:45 +0100

    styling and layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,13 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [mdx(), sitemap()],
+  integrations: [
+    mdx({
+      shikiConfig: {
+        theme: "catppuccin-mocha",
+      },
+    }),
+    sitemap(),
+  ],
   vite: { plugins: [tailwindcss()] },
 });
